5feb9efc03b4634d1107df33ee6e0c355ae8004b
baiching <bachmarma@gmail.com>
2025-12-02 14:55:26 +0100

    updated the documentation and gitignore

--- src/tpool.h
+++ src/tpool.h
@@ -29,14 +29,34 @@ tpool_t *f_tpool_create(int num_of_threads, int queue_size);
  *
  * @param pool:         the pointer to the thread pool
  * @param function:     the function pointer points to the function that needed to run
+ * @param arg:          the argument that needs to be passed to the function
  *
  * @return 0 on Success and -1 on failure
  *
  */
-int f_tpool_add(tpool_t *pool, void (*function)(void *, void *));
+int f_tpool_add(tpool_t *pool, void (*function)(void *), void *arg);
 
-int f_tpool_done(tpool_t *pool, int maxoutput, int timeout);
+/**
+ * @brief Adds new taks in the queue
+ *
+ * @param pool:         the pointer to the thread pool
+ * @param output:       It points to the completed tasks results
+ * @param maxoutput:    total results could be returned at once
+ *
+ * @return              number of completed tasks
+ *
+ */
+int f_tpool_done(tpool_t *pool, void **output, int maxoutput);
 
+
+/**
+ * @brief Finishes all unfished tasks and destroy the thread pool
+ *
+ * @param pool:         the thread pool that required to destroy
+ *
+ * @return              0 on Success and -1 on failure
+ *
+ */
 int f_tpool_destroy(tpool_t *pool);
 
 #ifdef __cplusplus
